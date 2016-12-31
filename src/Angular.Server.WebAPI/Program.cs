20f38a9dfcac580aa660fb938e23c2dfe9a3ffc0
Ivaylo Botusharov <encounter12@gmail.com>
2016-12-31 16:24:30 +0200

    Moved server urls to hosting.json

--- src/Angular.Server.WebAPI/Program.cs
+++ src/Angular.Server.WebAPI/Program.cs
@@ -2,15 +2,31 @@
 {
     using System.IO;
     using Microsoft.AspNetCore.Hosting;
+    using System.Collections.Generic;
+    using Microsoft.Extensions.Configuration;
 
     public class Program
     {
+        private static readonly Dictionary<string, string> defaults =
+            new Dictionary<string, string>
+            {
+                { WebHostDefaults.EnvironmentKey, "Development" }
+            };
+
         public static void Main(string[] args)
         {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: true)
+                .AddInMemoryCollection(defaults)
+                .AddEnvironmentVariables("ASPNETCORE_ENVIRONMENT")
+                .AddCommandLine(args)
+                .Build();
+
             var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseUrls("http://localhost:5000")
+                .UseConfiguration(configuration)
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseKestrel()
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
